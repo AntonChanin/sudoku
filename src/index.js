ba285aaa21f91336fdd56e84772ecdee346280d5
antonvh <antonhaninvl@gmail.com>
2019-10-13 20:48:40 +0300

    feat: add nonPossibilites

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
+  let nonPossibilites = {};
+  for (let vert = 0; vert < matrix.length; vert++) {
+  	for (let horz= 0; i < matrix.length; horz++) {
+  		if (matrix[vert][horz] === 0) {
+  			nonPossibilites = {};
+  			for (let i = 0; i < 10; i++) {
+  				if (matrix[vert][horz] > 0) {
+  					nonPossibilites[matrix[vert][horz]] = true;
+  				}
+  			}
+  		}
+  	}
+  }
+
 }
